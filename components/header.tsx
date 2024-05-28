f77ef67b01df3b22cb92052ef972a4847eb1bd83
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-05-28 18:29:23 +0530

    User logo added to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,7 @@
+import { UserButton, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import HeaderLogo from "./header-logo";
 import Navigation from "./navigation";
+import { Loader2 } from "lucide-react";
 
 const Header = () => {
   return (
@@ -10,6 +12,12 @@ const Header = () => {
             <HeaderLogo />
             <Navigation />
           </div>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-100" />
+          </ClerkLoading>
         </div>
       </div>
     </header>
