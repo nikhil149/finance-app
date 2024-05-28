86ba6c7d299dbd15e525d848dcfec782da19354b
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-05-28 17:45:31 +0530

    Header logo created for web version

---
+++ components/header-logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const HeaderLogo = () => {
+  return (
+    <Link href="/">
+      <div className="items-center hidden lg:flex">
+        <Image alt="Logo" src="/logo.svg" height={28} width={28} />
+        <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
+      </div>
+    </Link>
+  );
+};
+
+export default HeaderLogo;
