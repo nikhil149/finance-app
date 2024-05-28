f00986a594de1d4a1d2e1d25400c6adf5033f086
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-05-28 16:49:47 +0530

    UserButton added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is Main Page
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
