420fc66b18849b378c2a9a955280c180f00b4cd7
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-06-01 14:35:54 +0530

    hook removed from page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 export default function Home() {
-  const { data: accounts, isLoading } = useGetAccounts();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      {accounts?.map((account) => (
-        <div key={account.id}>{account.name}</div>
-      ))}
+      Home Page
     </div>
   );
 }
