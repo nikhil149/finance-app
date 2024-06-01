f69933c2332a73fe6c1b0178ab6ee1e41b9dd6a6
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-06-01 13:29:09 +0530

    Apptype and select query added

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
 
-const app = new Hono();
-
-app.get("/", (c) => {
-  return c.json({ accounts: [] });
+const app = new Hono().get("/", async (c) => {
+  const data = await db
+    .select({ id: accounts.id, name: accounts.name })
+    .from(accounts);
+  return c.json({ data });
 });
 
 export default app;
