b1d6bc75b417c8a2953c4c26c99303c456dd003f
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-06-03 16:59:41 +0530

    Table and pagination added to accounts section

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Payment, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
+
+const AccountsPage = () => {
+  const newAccount = useNewAccount();
+  return (
+    <div className="max-w-screen-2xl mx-auto pb-10 w-full -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Accounts Page</CardTitle>
+          <Button onClick={newAccount.onOpen}>
+            <Plus className="mr-2 size-4" /> Add New
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AccountsPage;
